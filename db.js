724ca8ebe200b34a3adef2c6a255f784cd78630c
Halothus <eduleon94@gmail.com>
2020-06-25 21:30:14 -0300

    Added more evente, cart total and price total

--- db.js
+++ db.js
@@ -4,49 +4,57 @@ function Db() {
             id: 1,
             name: "Metallica",
             src: "images/metallica.jpg",
-            price: 10
+            price: 10,
+            music: "music/metallica.mp3"
         },
         2:{
             id: 2,
             name: "Pink Floyd",
             src: "images/pink.jpg",
-            price: 10
+            price: 10,
+            music: "music/floyd.mp3"
         },
         3:{
             id: 3,
             name: "Guns N' Roses",
             src: "images/guns.jpg",
-            price: 10
+            price: 10,
+            music: "music/guns.mp3"
         },
         4:{
             id: 4,
             name: "The Beatles",
             src: "images/beatles.jpg",
-            price: 10
+            price: 10,
+            music: "music/beatles.mp3"
         },
         5:{
             id: 5,
             name: "Rolling Stones",
             src: "images/rolling.jpg",
-            price: 10
+            price: 10,
+            music: "music/stones.mp3"
         },
         6:{
             id: 6,
             name: "Led Zepellin",
             src: "images/led.jpg",
-            price: 10
+            price: 10,
+            music: "music/zepellin.mp3"
         },
         7:{
             id: 7,
             name: "Queen",
             src: "images/queen.jpg",
-            price: 10
+            price: 10,
+            music: "music/queen.mp3"
         },
         8:{
             id: 8,
             name: "Pearl Jam",
             src: "images/pearl.jpg",
-            price: 10
+            price: 10,
+            music: "music/pearl.mp3"
         },
         
     }    
--- main.js
+++ main.js
@@ -1,39 +1,61 @@
 // @ts-check
 // SHOPPING CART
+
 function ShoppingCart() {
     // { id: numero de discos en el carrito }
-    let cart = {};
+     let cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = {}
+        localStorage.setItem('cart', JSON.stringify({}))
+    }
 
     this.addOne = function(id) {
         if (cart[id])
             cart[id] += 1;
-        else
+        else 
             cart[id] = 1;
-        
+
+        localStorage.setItem('cart', JSON.stringify(cart))
         console.log("Producto con id:", id, ". Tienes un total de: ", cart[id], "de estos productos en el carrito");
+        console.log("Total: $", this.getTotalAmount())
         return cart[id];
     }
 
     this.removeOne = function(id) {
         if (cart[id] && cart[id] >= 1) {
             cart[id] -= 1;
+            localStorage.setItem('cart', JSON.stringify(cart))
             console.log("Producto con id:", id, "removido del carrito con un total de", cart[id], "restantes de este producto");
+            console.log("Total: $", this.getTotalAmount())
         }
         return cart[id];
-    }
+    }   
 
     this.get = function(){
         return cart;
     }
 
-    this.getTotalAmount = function(){
-        let total = 0;
-        cart.forEach(currentProduct => {
-            total += currentProduct.price
-        })
-        return total;
+    this.getTotalById = function(id) {
+        return cart[id] || 0
     }
 
+    this.getTotalAmount = function(){
+        let total = 0;
+        Object.keys(cart).forEach(productId => {
+            const price = db.getById(productId).price
+            const totalAmount = cart[productId]
+            total += price * totalAmount
+        })
+        return total;    
+    }
+    this.getTotalProducts = function(){
+        let total = 0;
+        Object.keys(cart).forEach(productId => {
+            total += cart[productId]
+        })  
+        return total; 
+    }   
 }
 
 function Product(id, name, price) {
@@ -70,11 +92,20 @@ const catalogoEl = document.getElementById('catalogo');
 
 db.get().forEach(item => {
     const productoEl = document.createElement("div");
-    productoEl.className = "w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col";
+    productoEl.className = "bg-gray-900 w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col ";
+    
+    const audioEl = document.createElement('audio');
+    const sourceEl = document.createElement('source');
+    sourceEl.src = item.music;
+    sourceEl.type = "audio/mp3";
 
     // image
     const discEl = document.createElement("img");
+    discEl.className = "records";
     discEl.src = item.src;
+    discEl.onmouseover = () => audioEl.play()
+    discEl.onmouseout = () => audioEl.pause()
+    
     
     // title
     const titleEl = document.createElement("div");
@@ -83,35 +114,47 @@ db.get().forEach(item => {
     const nameEl = document.createElement("span");
     nameEl.textContent = item.name;
 
+    
     const cartControlsEl = document.createElement("div");
     cartControlsEl.className = "flex items-center justify-center";
 
     // Add 
     const addButtonEl = document.createElement("button");
+    
     // total
     const totalEl = document.createElement("span");
+    const total = myShoppingCart.getTotalById(item.id);
     totalEl.id = "total-" + item.id;
-    totalEl.textContent = "0";
-    totalEl.className = "hidden";
+    totalEl.textContent = total;
 
     // remove
     const removeButtonEl = document.createElement("button");
-    removeButtonEl.className = "hidden";
+    
+    if (total > 0) {
+        removeButtonEl.className = "block"        
+        totalEl.className = "block px-2";
+    }
+    else {
+        removeButtonEl.className = "hidden";
+        totalEl.className = "hidden";
+    }
 
     addButtonEl.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="fill-current text-yellow-900">
-            <path class="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-9h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2V9a1 1 0 0 1 2 0v2z"/>
-        </svg>
-    `
+            <path class="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-9h2a1 1 0 0 1 0 2h-2v2a1
+             1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2V9a1 1 0 0 1 2 0v2z"/>
+        </svg>`
+
     addButtonEl.addEventListener('click', () => {
         const newTotal = myShoppingCart.addOne(item.id);
         totalEl.textContent = newTotal;
         if (newTotal === 1) {
-            totalEl.className = "visible px-2";
-            removeButtonEl.className = "visible";
+            totalEl.className = "block px-2";
+            removeButtonEl.className = "block";
+   
         }
     });
-
+   
     removeButtonEl.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="fill-current text-red-900" style="transform: rotate(45deg);">
             <path class="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-9h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2V9a1 1 0 0 1 2 0v2z"/>
@@ -123,16 +166,16 @@ db.get().forEach(item => {
         if (newTotal === 0) {
             totalEl.className = "hidden";
             removeButtonEl.className = "hidden";
+           
         }
     });
-
+    
     cartControlsEl.appendChild(removeButtonEl);
     cartControlsEl.appendChild(totalEl);
     cartControlsEl.appendChild(addButtonEl);
 
     titleEl.appendChild(nameEl);
     titleEl.appendChild(cartControlsEl);
-
 
     // precio
     const priceEl = document.createElement("span");
@@ -141,6 +184,6 @@ db.get().forEach(item => {
     productoEl.appendChild(discEl);
     productoEl.appendChild(titleEl);
     productoEl.appendChild(priceEl);
-
+    audioEl.appendChild(sourceEl);
     catalogoEl.appendChild(productoEl);
 })
